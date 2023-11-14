d11b3b5c65ab16aee1263f1e65a9d64e47eb305b
Ayush Thakur <testmailforyt12@gmail.com>
2023-11-14 23:53:10 +0530

    Initial Commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    import { count } from '$lib';
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<h1>{$count}</h1>
+<button on:click={() => count.update(n => n+1)}>
+    Increment
+</button>
